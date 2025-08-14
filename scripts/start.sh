3ca79a3ac21c43076813d5d2c0b6f8229588a582
facehint <facehint@gmail.com>
2025-08-14 08:24:59 +0800

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 source /home/ecs-user/facehint-api/bin/activate
-cd /home/ecs-user/app/app
+cd /home/ecs-user/app
 python -m uvicorn --app-dir /home/ecs-user/app  main:app --reload --host 0.0.0.0 --port 8080
 #python -m uvicorn --app-dir /home/ecs-user/app  main:app --reload --host 0.0.0.0 --port 443 --ssl-keyfile /etc/letsencrypt/live/api.facehint.xyz/privkey.pem --ssl-certfile /etc/letsencrypt/live/api.facehint.xyz/fullchain.pem
